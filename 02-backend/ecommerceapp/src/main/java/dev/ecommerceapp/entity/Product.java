57000eb8ae3c36ca0b8c998f980b793b3f04aeb4
revirste <145519772+revirste@users.noreply.github.com>
2024-12-27 15:23:43 -0500

    Removed Lombok and created explicit Getters, Setters, equals, hashCode, and toString.

--- 02-backend/ecommerceapp/src/main/java/dev/ecommerceapp/entity/Product.java
+++ 02-backend/ecommerceapp/src/main/java/dev/ecommerceapp/entity/Product.java
@@ -2,35 +2,146 @@ package dev.ecommerceapp.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.Data;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
 @Entity
 @Table(name="product")
-@Data
 public class Product {
 
     private Long id;
-
     private String sku;
-
     private String name;
-
     private String description;
-
     private BigDecimal unitPrice;
-
     private String imageUrl;
-
     private Boolean active;
-
     private int unitsInStock;
-
     private Date dateCreated;
-
     private Date lastUpdated;
 
+    public Product(Long id, String sku, String name, String description, BigDecimal unitPrice,
+                   String imageUrl, Boolean active, int unitsInStock, Date dateCreated, Date lastUpdated) {
+        this.id = id;
+        this.sku = sku;
+        this.name = name;
+        this.description = description;
+        this.unitPrice = unitPrice;
+        this.imageUrl = imageUrl;
+        this.active = active;
+        this.unitsInStock = unitsInStock;
+        this.dateCreated = dateCreated;
+        this.lastUpdated = lastUpdated;
+    }
 
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSku() {
+        return this.sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return this.unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getImageUrl() {
+        return this.imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Boolean getActive() {
+        return this.active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public int getUnitsInStock() {
+        return this.unitsInStock;
+    }
+
+    public void setUnitsInStock(int unitsInStock) {
+        this.unitsInStock = unitsInStock;
+    }
+
+    public Date getDateCreated() {
+        return this.dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getLastUpdated() {
+        return this.lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+        return id.equals(product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", sku='" + sku + '\'' +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", unitPrice=" + unitPrice +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", active=" + active +
+                ", unitsInStock=" + unitsInStock +
+                ", dateCreated=" + dateCreated +
+                ", lastUpdated=" + lastUpdated +
+                '}';
+    }
 }
--- 02-backend/ecommerceapp/src/main/java/dev/ecommerceapp/entity/ProductCategory.java
+++ 02-backend/ecommerceapp/src/main/java/dev/ecommerceapp/entity/ProductCategory.java
@@ -2,12 +2,53 @@ package dev.ecommerceapp.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import lombok.Data;
 
 @Entity
 @Table(name="product_category")
-@Data
 public class ProductCategory {
+
     private Long id;
     private String categoryName;
+
+    public ProductCategory(Long id, String categoryName) {
+        this.id = id;
+        this.categoryName = categoryName;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return this.categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ProductCategory that = (ProductCategory) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "ProductCategory{" +
+                "id=" + id +
+                ", categoryName='" + categoryName + '\'' +
+                '}';
+    }
 }
