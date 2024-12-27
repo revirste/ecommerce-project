fe05c72640c7a9014168f351580228c88863ffe1
revirste <145519772+revirste@users.noreply.github.com>
2024-12-27 16:56:41 -0500

    The API has been converted to be read only - Implemented configureRepositoryRestConfiguration() method from the RepositoryRestConfigurer interface. Set PUT, POST, and DELETE as unsupported actions in the REST Http exposure configuration

---
+++ 02-backend/ecommerceapp/src/main/java/dev/ecommerceapp/config/MyDataRestConfig.java
@@ -0,0 +1,32 @@
+package dev.ecommerceapp.config;
+
+import dev.ecommerceapp.entity.Product;
+import dev.ecommerceapp.entity.ProductCategory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+
+@Configuration
+public class MyDataRestConfig implements RepositoryRestConfigurer {
+
+    @Override
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+
+        //  HTTP methods except for GET
+        HttpMethod[] unsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE};
+
+        // Disable those HTTP methods for Product entity
+        config.getExposureConfiguration()
+                .forDomainType(Product.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+
+        // Disable those HTTP methods for ProductCategory entity
+        config.getExposureConfiguration()
+                .forDomainType(ProductCategory.class)
+                .withItemExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions))
+                .withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(unsupportedActions));
+    }
+}
