dc5d814f7e5d671a1293ca1d137451798f8b3f58
revirste <145519772+revirste@users.noreply.github.com>
2024-12-30 19:29:56 -0500

    Enhanced the product service such that it makes an HTTP GET request to the Spring Boot backend for /api/products. The returned json is mapped to an array of products for ease of access

--- 03-frontend/ecommerce-app/src/app/services/product.service.ts
+++ 03-frontend/ecommerce-app/src/app/services/product.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Product } from '../common/Product';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private baseUrl: string = 'http://localhost:8080/api/products';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getProductList(): Observable<Product[]> {
+    return this.httpClient.get<GetResponse>(this.baseUrl).pipe(map(response => response._embedded.products)
+  }
+}
+
+interface GetResponse {
+  _embedded: {
+    products: Product[];
+  }
 }
